1137d07c90455c01c99cf5a0ecd129e04d74555e
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2022-03-01 19:08:58 -0600

    Initial Commit
    Initial Commit

---
+++ encounterlink.js
@@ -0,0 +1,8 @@
+function change(){
+    document.getElementById("header_encounter").innerHTML ="Encounter with Pikachu";
+    document.getElementById("p_encounter").innerHTML=" You are starting your journey as a Pokémon trainer, as soon as you leave your house you encounter a wild Pikachu in the tall grass! It's time to catch your first Pokémon.";
+    document.getElementById("caption_encounter").innerHTML="Copyright: The Pokémon Company Inc./Game Freak Co., Ltd/ Nintendo Co., Ltd";
+    document.getElementById("link").innerHTML="Throw Pokéball";
+    document.getElementById("label_file").innerHTML="Story progress";
+    document.getElementById("file").value = 12.50;
+}
---
+++ indexlink.js
@@ -0,0 +1,11 @@
+function change(){
+        document.getElementById("title_index").innerHTML = "Lab 06: Pokémon";
+        document.getElementById("p_index").innerHTML = "This is the index for my first HTML story-telling the encounter with a wild Pikachu and the first Rival Battle with some decision making dynamics....";
+        document.getElementById("p2_index").innerHTML =  "Should we start?";
+        document.getElementById("link").innerHTML = "Start Story";
+        document.getElementById("label_progress").innerHTML ="Story progress";
+        document.getElementById("file").value = 0;
+}
+
+
+
